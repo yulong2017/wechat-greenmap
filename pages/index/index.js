701a09cee2986854ecfe6e8a09f192849c77799e
yulong2017 <merida550@163.com>
2018-11-18 18:44:12 +0800

    create

---
+++ pages/index/index.js
@@ -0,0 +1,49 @@
+var markersData = [];
+Page({
+  data: {
+    latitude: 39,
+    longitude: 116,
+    markers: [{
+      id: 0,
+      iconPath: "../../images/icon_cur_position.png",
+      latitude: 23.099994,
+      longitude: 113.324520,
+      width: 50,
+      height: 50
+    }]
+  },
+  makertap: function (e) {
+    var id = e.markerId;
+    var that = this;
+    that.showMarkerInfo(markersData, id);
+    that.changeMarkerColor(markersData, id);
+  },
+  onLoad: function () {
+    var that = this;
+    wx.getLocation({
+      type: "wgs84",
+      success: function (res) {
+        var latitude = res.latitude;
+        var longitude = res.longitude;
+        //console.log(res.latitude);
+        that.setData({
+          latitude: res.latitude,
+          longitude: res.longitude,
+          markers: [{
+            latitude: res.latitude,
+            longitude: res.longitude
+          }]
+        })
+      }
+    })
+  }, 
+  openLocation() {
+    wx.openLocation({ 
+      latitude: this.latitude, 
+      longitude: this.longitude, 
+      scale: 28, 
+      name: '西部电信中心', 
+      address: '益州大道1666号' 
+    })
+  }
+})
---
+++ pages/me/me.js
@@ -0,0 +1,37 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {}
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+  },
+  calling: function () {
+    wx.makePhoneCall({
+      phoneNumber: '13190179329', //此号码并非真实电话号码，仅用于测试
+      success: function () {
+        console.log("拨打电话成功！")
+      },
+      fail: function () {
+        console.log("拨打电话失败！")
+      }
+    })
+  }
+})
